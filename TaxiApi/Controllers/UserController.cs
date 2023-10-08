c16bfe5f3cf1fcd34a203bfd1f8b4261c72660a1
balachandarj <balachandarjegan@gmail.com>
2023-10-08 07:26:47 -0700

    Add project files.

---
+++ TaxiApi/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaxiApi.Models;
+
+namespace TaxiApi.Controllers
+{
+    [ApiController]
+    [Route("[UserController]")]
+    public class UserController : ControllerBase
+    {
+        private List<User> _users = new List<User>();
+
+
+        //[HttpPost]
+        //[Route("AddUser")]
+        //public string AddUser(User user)
+        //{
+        //    _users.Add(user);
+        //    return "Success";
+        //}
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                //Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
